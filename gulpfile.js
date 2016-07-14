0876c0929847419c4beee786b67ecc20c4e79b87
Manuel <manudzmz@gmail.com>
2016-07-15 00:57:44 +0200

    Añadidas notificaciones y errores al gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // importamos gulp
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var notify = require('gulp-notify');
 
 // definimos la tarea por defecto
 gulp.task('default', function(){
@@ -9,7 +10,11 @@ gulp.task('default', function(){
 
 // definimos la tarea para compilar Sass
 gulp.task('compile-sass', function(){
-	gulp.src('./src/scss/style.scss')  // cargamos el archivo
-	.pipe(sass())                    // compilamos el archivo sass
-	.pipe(gulp.dest('./dist/css'))   // guardamos el archivo en dist/css
+	gulp.src('./src/scss/style.scss')         // cargamos el archivo
+	.pipe(sass().on('error', sass.logError))  // compilamos el archivo sass
+	.pipe(gulp.dest('./dist/css'))            // guardamos el archivo en dist/css
+	.pipe(notify({
+		title: "SASS",
+		message: "Compilado"
+	}));
 });
